9df9c04b3bed54adea794422c54272514ff93789
belen <anabelenarroyo21@gmail.com>
2022-09-11 14:49:28 -0500

    creada función findLinks

--- functions.js
+++ functions.js
@@ -15,12 +15,18 @@ const isExtNameMd = (param) => path.extname(param) === '.md';
 
 const fileContent = (param) => fs.readFileSync(param, 'utf-8');
 
+const findLinks = (param) =>{
+    const regExp = /(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})/gi;
+    return param.match(regExp)
+}
+
 module.exports = {
     isPathAbsolute,
     toAbsolute,
     isExists,
     isDirectory,
     isExtNameMd,
-    fileContent
+    fileContent,
+    findLinks
 }
 
