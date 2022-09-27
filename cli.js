914aec1258d611e7c9ebcfee4b92ec9ba7ad9d16
belen <anabelenarroyo21@gmail.com>
2022-09-27 17:27:21 -0500

    avance porcess.argv

--- cli.js
+++ cli.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
 const functions = require("./index.js");
-const { argv } = require("yargs");
 const mdLinks = functions.mdLinks;
+//const { argv } = require("yargs");
 
 //TODO:
 //1. configurar cli
 //2. revisar la cantidad de argumentos que recibe el comando (mdlinks miruta) -> argv
+console.log(process.argv)
+const validateOption = process.argv.includes("--validate")
+
 //3. una vez validados los argumentos, llamar funcion mdLinks
 
-
-mdLinks("./readme.md", {validate:true})
+mdLinks("./readme.md",{validate:validateOption})
   .then((res) => console.log(res))
   .catch((res) => console.log(res));
+
+
+
