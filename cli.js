e0357b545477398fbebde3877ba44c6adbadc2fc
belen <anabelenarroyo21@gmail.com>
2022-09-29 12:01:49 -0500

    agregado manejo de argumento --validate

--- cli.js
+++ cli.js
@@ -2,22 +2,17 @@
 
 const functions = require("./index.js");
 const mdLinks = functions.mdLinks;
-//const { argv } = require("yargs");
 
 //TODO:
 //1. configurar cli
-//2. revisar la cantidad de argumentos que recibe el comando (mdlinks miruta) -> argv
 
-console.log(process.argv)
+//2. revisar la cantidad de argumentos que recibe el comando (mdlinks miruta) -> argv
+//console.log(process.argv)
 const [ , , route, option] = process.argv
-if(validateOption = process.argv.includes("--validate:true")){
-  const validateOption = true;
-}else if(validateOption = process.argv.includes("--validate:false")){
-  validateOption = false;
-}
+validateOption = process.argv.includes("--validate")
+
 
 //3. una vez validados los argumentos, llamar funcion mdLinks
-
 mdLinks(route,{validate:validateOption})
   .then((res) => console.log(res))
   .catch((res) => console.log(res));
